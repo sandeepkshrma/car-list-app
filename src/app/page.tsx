f0af112bf59482a318d4a8ccc79f6858b9bc1108
Sandeep Sharma <sandeepsharma@Sandeeps-MacBook-Air.local>
2025-04-24 16:47:08 +0530

    Updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
             alt="File icon"
             width={500}
             height={500}
+            unoptimized
           />
      <div/>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
